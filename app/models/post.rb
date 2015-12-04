ecee95f8e07c74971c3a94b0321847927d87b0d3
arialblack14 <tsirasalexis@yahoo.gr>
2015-12-04 19:43:37 +0200

    Add comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 150 },
                     uniqueness: { case_sensitive: false }
   validates :body, presence: true, length: { maximum: 700 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203215604) do
+ActiveRecord::Schema.define(version: 20151204163443) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
